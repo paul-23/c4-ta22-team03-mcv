8e01a68a1c1725ca80710408110bd66d0849a531
Paul S <62121921+paul-23@users.noreply.github.com>
2023-04-11 09:24:41 +0200

    Commit inicial

---
+++ ta22_c4_mvc_ejercicio03/src/main/java/team03/ta22_c4_mvc_ejercicio03/App.java
@@ -0,0 +1,13 @@
+package team03.ta22_c4_mvc_ejercicio03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
