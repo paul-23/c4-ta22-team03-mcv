3c029be71c3276e8a222f156582c76e140a5a110
Paul S <62121921+paul-23@users.noreply.github.com>
2023-04-19 00:20:20 +0200

    diseño seleccionar proyecto

--- ta22_c4_mvc_ejercicio03/src/main/java/views/vistaSeleccionAsignacion.java
+++ ta22_c4_mvc_ejercicio03/src/main/java/views/vistaSeleccionAsignacion.java
@@ -28,48 +28,49 @@ public class vistaSeleccionAsignacion extends JFrame {
 	 */
 	public vistaSeleccionAsignacion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 374, 255);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("Selecciona un Proyecto");
 		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 20));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(70, 42, 255, 57);
+		lblNewLabel.setBounds(54, 11, 259, 29);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("ID Proyecto");
 		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(10, 156, 132, 29);
+		lblNewLabel_1.setBounds(34, 65, 121, 24);
 		contentPane.add(lblNewLabel_1);
 		
 		textFieldIDAsignacion = new JTextField();
 		textFieldIDAsignacion.setFont(new Font("Arial", Font.PLAIN, 20));
-		textFieldIDAsignacion.setBounds(164, 147, 197, 46);
+		textFieldIDAsignacion.setBounds(165, 62, 176, 30);
 		contentPane.add(textFieldIDAsignacion);
 		textFieldIDAsignacion.setColumns(10);
 		
 		JLabel lblNewLabel_11 = new JLabel("DNI Cientifico");
 		lblNewLabel_11.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_11.setBounds(10, 156, 132, 29);
+		lblNewLabel_11.setBounds(34, 103, 121, 24);
 		contentPane.add(lblNewLabel_11);
 		
 		textFieldDNIAsignacion = new JTextField();
 		textFieldDNIAsignacion.setFont(new Font("Arial", Font.PLAIN, 20));
-		textFieldDNIAsignacion.setBounds(164, 147, 197, 46);
+		textFieldDNIAsignacion.setBounds(165, 100, 176, 30);
 		contentPane.add(textFieldDNIAsignacion);
 		textFieldDNIAsignacion.setColumns(10);
 		
 		btnBuscar = new JButton("Buscar");
 		btnBuscar.setFont(new Font("Arial", Font.PLAIN, 20));
-		btnBuscar.setBounds(38, 249, 150, 46);
+		btnBuscar.setBounds(69, 153, 95, 33);
 		contentPane.add(btnBuscar);
 		
 		btnCancelar = new JButton("Cancelar");
 		btnCancelar.setFont(new Font("Arial", Font.PLAIN, 20));
-		btnCancelar.setBounds(211, 249, 150, 46);
+		btnCancelar.setBounds(169, 153, 111, 33);
 		contentPane.add(btnCancelar);
 	}
 
