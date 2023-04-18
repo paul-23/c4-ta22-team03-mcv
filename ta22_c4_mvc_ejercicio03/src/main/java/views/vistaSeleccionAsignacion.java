4af644b39bf4c1d749ef2b616681699ca37f0ac2
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-04-18 22:50:35 +0200

    push seleccionasignacion

--- ta22_c4_mvc_ejercicio03/src/main/java/views/vistaSeleccionAsignacion.java
+++ ta22_c4_mvc_ejercicio03/src/main/java/views/vistaSeleccionAsignacion.java
@@ -1,30 +1,26 @@
 package views;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class vistaSeleccionAsignacion extends JFrame {
 
 	private JPanel contentPane;
-
+	public JTextField textFieldIDAsignacion;
+	public JButton btnBuscar;
+	public JButton btnCancelar;
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					vistaSeleccionAsignacion frame = new vistaSeleccionAsignacion();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
@@ -36,6 +32,33 @@ public class vistaSeleccionAsignacion extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+
+		JLabel lblNewLabel = new JLabel("Selecciona un Proyecto");
+		lblNewLabel.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(70, 42, 255, 57);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("ID Proyecto");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(10, 156, 132, 29);
+		contentPane.add(lblNewLabel_1);
+		
+		 textFieldIDAsignacion = new JTextField();
+		textFieldIDAsignacion.setFont(new Font("Arial", Font.PLAIN, 20));
+		textFieldIDAsignacion.setBounds(164, 147, 197, 46);
+		contentPane.add(textFieldIDAsignacion);
+		textFieldIDAsignacion.setColumns(10);
+		
+		btnBuscar = new JButton("Buscar");
+		btnBuscar.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnBuscar.setBounds(38, 249, 150, 46);
+		contentPane.add(btnBuscar);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.setFont(new Font("Arial", Font.PLAIN, 20));
+		btnCancelar.setBounds(211, 249, 150, 46);
+		contentPane.add(btnCancelar);
 	}
 
 }
